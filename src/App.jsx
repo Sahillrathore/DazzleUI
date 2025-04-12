4413fde16b83d007eb2f5eaccf6a6dc61e33cc51
Sahillrathore <sahil921031@gmail.com>
2025-04-12 16:37:32 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Homepage from './pages/Homepage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Elements from './pages/Elements'
+import Header from './components/Header'
+import ProtectedRoute from './components/ProtectedRoute'
+import CreatePage from './pages/CreatePage'
+import Login from './pages/Login'
 
 const App = () => {
   return (
     <div>
-      <Homepage/>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/elements" element={<Elements />} />
+        <Route path="/create" element={<ProtectedRoute><CreatePage /></ProtectedRoute>} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   )
 }
