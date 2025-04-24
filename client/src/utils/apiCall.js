41973ac53597184cc8eac3ff97733ba229e29fde
Sahillrathore <sahil921031@gmail.com>
2025-04-24 17:40:22 +0530

    custom function

---
+++ client/src/utils/apiCall.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+//to get all the elements
+export const getAllElements = async () => {
+    const res = await axios.get(import.meta.env.VITE_BACKEND_URL+"/api/elements", {
+        withCredentials: true,
+    });
+    return res.data;
+};
+
+//to get all the elements of particular user
+export const getUserElements = async (userId) => {
+    const res = await axios.get(import.meta.env.VITE_BACKEND_URL+`/api/elements/user/${userId}`, {
+        withCredentials: true,
+    });
+    return res.data;
+};
